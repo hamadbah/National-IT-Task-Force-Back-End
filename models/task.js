13a0fba6ae88688e76ceabbb4112fc52d833adb9
Alhawari80 <anahawari80@gmail.com>
2025-08-04 20:19:22 +0300

    task model

---
+++ models/task.js
@@ -0,0 +1,37 @@
+// models/hoot.js
+const mongoose = require('mongoose');
+
+//
+
+const taskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+      enum: ['1-3 months', '3-6 months', '6-12 months', '1 year or more'],
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    ministry: {
+      type: ObjectId,
+      required: true,
+    },
+    teams: {
+      type: ObjectId,
+      required: true,
+    },
+  }
+);
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
