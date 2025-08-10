2bc10e1082daaefd8aee0cf9fdeb2729fff62fb0
hamadbah <hamad.coding@gmail.com>
2025-08-10 18:35:44 +0300

    Modify models and controllers to allow assigning tasks to ministry

--- controllers/ministry.js
+++ controllers/ministry.js
@@ -1,38 +1,31 @@
 const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
 const Ministry = require("../models/ministry.js");
+const Task = require('../models/task.js');
 const router = express.Router();
 
-//creating the Ministry using CRUD. 
 router.post("/", verifyToken, async (req, res) => {
   try {
     req.body.Owner = req.user._id;
     const ministry = await Ministry.create(req.body);
-    await ministry.populate('Owner')
     res.status(201).json(ministry);
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
 });
 
-//implementing the read 
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const ministries = await Ministry.find({})
-      .populate("Owner")
-      .sort({ createdAt: "desc" });//decending order. from newest to oldest
+    const ministries = await Ministry.find({}).populate('tasks');
     res.status(200).json(ministries);
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
 });
 
-//populating the owner of ministry 
 router.get('/:ministryId', verifyToken, async (req, res) => {
   try {
-    const ministry = await Ministry.findById(req.params.ministryId).populate([
-      'Owner',
-    ]);
+    const ministry = await Ministry.findById(req.params.ministryId).populate('tasks');
     res.status(200).json(ministry);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -41,24 +34,16 @@ router.get('/:ministryId', verifyToken, async (req, res) => {
 
 router.put("/:ministryId", verifyToken, async (req, res) => {
   try {
-    // Find the ministry using the ID
     const ministry = await Ministry.findById(req.params.ministryId);
-
-    // Checking the permissions:
     if (!ministry.Owner.equals(req.user._id)) {
       return res.status(403).send("You're not allowed to do that!");
     }
-
-    // Update Ministry:
     const updatedMinistry = await Ministry.findByIdAndUpdate(
       req.params.ministryId,
       req.body,
       { new: true }
     );
     updatedMinistry = await updatedMinistry.populate('Owner')
-
-
-    // Issue JSON response:
     res.status(200).json(updatedMinistry);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -68,15 +53,30 @@ router.put("/:ministryId", verifyToken, async (req, res) => {
 router.delete("/:ministryId", verifyToken, async (req, res) => {
   try {
     const ministry = await Ministry.findById(req.params.ministryId);
-
-    // if (!ministry.Owner.equals(req.user._id)) {
-    //   return res.status(403).send("You're not allowed to do that!");
-    // }
+    await Task.deleteMany({ ministry: { $in: [req.params.ministryId] } });
     const deletedMinistry = await Ministry.findByIdAndDelete(req.params.ministryId);
     res.status(200).json(deletedMinistry);
   } catch (err) {
     console.log(err);
-    
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.put("/:ministryId/tasks", verifyToken, async (req, res) => {
+  try {
+    const { taskIds } = req.body;
+    if (!Array.isArray(taskIds)) {
+      return res.status(400).json({ err: "taskIds must be an array" });
+    }
+    const ministry = await Ministry.findById(req.params.ministryId);
+    if (!ministry) {
+      return res.status(404).json({ err: "Ministry not found" });
+    }
+    ministry.tasks = taskIds;
+    await ministry.save();
+    await ministry.populate('tasks');
+    res.status(200).json(ministry);
+  } catch (err) {
     res.status(500).json({ err: err.message });
   }
 });
